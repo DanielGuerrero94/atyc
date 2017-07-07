f393747e4d9de72b09e51a5d0bcfd18a99c7e081
Daniel Guerrero <daniel.guerrero.94@gmail.com>
2017-07-07 18:38:06 -0300

    menu

---
+++ app/Menu.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'sistema.menues';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id_menu';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The model's attributes.
+     *
+     * @var array
+     */
+    protected $attributes = ['nombre','icono','color'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre','icono','color'];
+
+    public function subMenues()
+    {
+    	return $this->hasMany('sistema.menues','id_menu','id_padre')
+    }
+}
