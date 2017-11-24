4990ef27ca3639572a461decc4d9296bea4f957a
Daniel Guerrero <daguerrero@est.frba.utn.edu.ar>
2017-11-24 11:07:02 -0300

    Cambio en nombre de cookies del framework para corregir problema entre sistemas

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -12,6 +14,50 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        /*'areasTematicas/set'*/
+    	/*'areasTematicas/set'*/
     ];
+
+    /**
+     * Determine if the session and input CSRF tokens match.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function tokensMatch($request)
+    {
+    	$sessionToken = $request->session()->token();
+
+    	$token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN-ATYC');
+
+    	if (! $token && $header = $request->header('X-XSRF-TOKEN-ATYC')) {
+    		$token = $this->encrypter->decrypt($header);
+    	}
+
+    	if (! is_string($sessionToken) || ! is_string($token)) {
+    		return false;
+    	}
+
+    	return hash_equals($sessionToken, $token);
+    }
+
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function addCookieToResponse($request, $response)
+    {
+    	$config = config('session');
+
+    	$response->headers->setCookie(
+    		new Cookie(
+    			'XSRF-TOKEN-ATYC', $request->session()->token(), Carbon::now()->getTimestamp() + 60 * $config['lifetime'],
+    			$config['path'], $config['domain'], $config['secure'], false
+    		)
+    	);
+
+    	return $response;
+    }
 }
