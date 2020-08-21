988597a986e70c6b17e68e71cc372ee17ce1ee33
julianpebe <julianperezbadon@hotmail.com>
2020-08-21 01:43:52 -0300

    merge?

--- database/fix/alter_branch_reportes_merge.sql
+++ database/fix/alter_branch_reportes_merge.sql
@@ -1,12 +1,5 @@
 -- branch reportes-mergeados
--- update sistema.reportes set nombre = 'Reportes de PAC', view = 'reportes-pac' where id_reporte = 8;
+update sistema.reportes set nombre = 'Reportes de PAC', view = 'reportes-pac' where id_reporte = 8;
 
--- delete from sistema.reportes where id_reporte in (9, 12);
+delete from sistema.reportes where id_reporte in (9, 12);
 
--- branch master
-insert into sistema.reportes (nombre, view, created_at) values
-('Cantidad de Acciones Ejecutadas de PAC', 'ejecutadas-pac', now()),
-('Porcentaje de Acciones Ejecutadas de PAC', 'porcentaje-ejecutadas-de-planificadas', now());
-
-update sistema.reportes set id_reporte = 9 where id_reporte = 13;
-update sistema.reportes set id_reporte = 12 where id_reporte = 14;
