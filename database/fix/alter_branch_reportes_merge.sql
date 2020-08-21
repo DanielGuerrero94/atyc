cbb539f89f48b11c22af161e2860e777e684d1b0
julianpebe <julianperezbadon@hotmail.com>
2020-08-21 12:51:44 -0300

    File para volver database al estado anterior

--- database/fix/alter_branch_reportes_merge.sql
+++ database/fix/alter_branch_reportes_merge.sql
@@ -1,8 +1,16 @@
+
 -- branch master
+
+update sistema.reportes set id_reporte = 11 where id_reporte = 10;
+update sistema.reportes set id_reporte = 10 where id_reporte = 9;
+update sistema.reportes set nombre = 'Cantidad de Acciones Planificadas en PAC', view = 'planificadas-pac' where id_reporte = 8;
+
+
 insert into sistema.reportes (nombre, view, created_at) values
 ('Cantidad de Acciones Ejecutadas de PAC', 'ejecutadas-pac', now()),
 ('Porcentaje de Acciones Ejecutadas de PAC', 'porcentaje-ejecutadas-de-planificadas', now());
 
 update sistema.reportes set id_reporte = 9 where id_reporte = 13;
 update sistema.reportes set id_reporte = 12 where id_reporte = 14;
-update sistema.reportes set nombre = 'Cantidad de Acciones Planificadas en PAC', view = 'planificadas-pac' where id_reporte = 8;
+
+alter sequence sistema.reportes_id_reporte_seq restart with 12;
